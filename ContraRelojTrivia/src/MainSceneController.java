f31b5ffba60dc47688ee019e2a2114e709f64297
saramburo <106128587+saramburo@users.noreply.github.com>
2022-06-26 20:18:20 -0500

    Update MainSceneController.java

--- ContraRelojTrivia/src/MainSceneController.java
+++ ContraRelojTrivia/src/MainSceneController.java
@@ -1,17 +1,98 @@
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
-import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
 import javafx.stage.Stage;
 
-public class MainSceneController {
+import java.io.IOException;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+    @FXML
+    private Button bt_inicio;
+    private Stage stage;
+    private Scene scene;
+    private Parent root;
 
     @FXML
-    private TextField cambiarTitulo;
-
-    @FXML
-    void clickCambiarTituloBtn(ActionEvent event) {
-        Stage ventanaPrincipal = (Stage) cambiarTitulo.getScene().getWindow();
-        String titulo = cambiarTitulo.getText();
-        ventanaPrincipal.setTitle(titulo);
+    protected void onInicioButtonClick(ActionEvent event) throws IOException {
+     root=FXMLLoader.load(this.getClass().getResource("Temas.fxml"));
+     stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+     scene=new Scene(root);
+     stage.setScene(scene);
+     stage.show();
     }
-}
+    @FXML
+    protected void onGeografiaClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onEntretenimientoClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onHistoriaClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onArteClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onCienciaClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onDeportesClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+    @FXML
+    protected void onCulturaClick(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Preguntas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    @FXML
+    protected void onvolverTemas(ActionEvent event) throws IOException {
+        root=FXMLLoader.load(this.getClass().getResource("Temas.fxml"));
+        stage=(Stage)((Node)event.getSource()).getScene().getWindow();
+        scene=new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+
